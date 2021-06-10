d7d4f6e524351619c77c2abfdae83447aa10bb5e
SvetlanaMelin <84378309+SvetlanaMelin@users.noreply.github.com>
2021-06-10 22:48:02 +0000

    I added my personal solution

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,30 +1,43 @@
-const input = require('readline-sync');
+   
+const quiz = require('./candidate-testing');
 
-// TODO 2: modify your quiz app to ask 5 questions //
-
-// TODO 1.1a: Define candidateName // 
-let candidateName;
-
+let question;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
-let correctAnswer1 = "sally ride";
-let candidateAnswer = '';
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions =['1. Who was the first American woman in space?','2. True or false: 5 kilometer == 5000 meters?','3. (5 + 3)/2 * 10 = ?',"4. Given the array [8, 'Orbit', 'Trajectory',45], what entry is at index 2?",'5. What is the minimum crew size for the ISS?'];
+let correctAnswers=["Sally Ride","true","40","Trajectory","3"];
+let candidateAnswers ='';
+let candidateName;
+//let candidateName;
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+//let question = "Who was the first American woman in space? ";
+////let correctAnswer = "Sally Ride";
+//let correctAnswer1 = "sally ride";
+//let candidateAnswer = '';
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //k
-candidateName = input.question('What is your name? ');
+  
+const input = require('readline-sync');
+candidateName = input.question('What is your name candidate? ');
 
 }
+
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
  
-candidateAnswer = input.question(question);
+//candidateAnswer = input.question(question);
 
+const input = require('readline-sync');
+
+for (let i=0;i < questions.length;i++) {
+
+console.log(questions[i]);
+candidateAnswers[i] = input.question('Your answer: ');
+console.log(`Correct answer is: ${correctAnswers[i]}\n`)
+}
+//console.log(`Your answer: ${candidateAnswers}`);
+//conole.log(`Correct aswer:${correctAnswers}`);
 }
 
 
@@ -32,14 +45,18 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 let grade;
-if (candidateAnswer === correctAnswer){
-  console.log("Your answer is correct!");
-} else if (candidateAnswer === correctAnswer1){
-  console.log("Your answer is correct!");
-} else 
-  {console.log('Your answer is wrong');
 
-}
+
+/*for ( let i=o;i<correctAnswers.length;i++){
+
+
+if (candidateAnswers === correctAnswers[i]){
+  console.log("Your answer is correct!");
+} 
+else 
+  {console.log('Your answer is wrong');
+  }}*/
+
   return grade;
 }
 
@@ -51,13 +68,11 @@ function runProgram() {
   gradeQuiz(this.candidateAnswers);
 }
 
-// Don't write any code below this line //
-// And don't change these or your program will not run as expected //
 module.exports = {
   candidateName: candidateName,
   question: question,
-  correctAnswer: correctAnswer,
-  candidateAnswer: candidateAnswer,
+  //correctAnswer: correctAnswer,
+  //candidateAnswer: candidateAnswer,
   questions: questions,
   correctAnswers: correctAnswers,
   candidateAnswers: candidateAnswers,
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+  
 const quiz = require('./candidate-testing');
 
 
 quiz.runProgram();
-
