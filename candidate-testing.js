75c6f733eeb042088d1e1866f9b626bcb4a30cfd
SvetlanaMelin <84378309+SvetlanaMelin@users.noreply.github.com>
2021-06-15 18:31:15 +0000

    Added my personal solution

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,8 +1,7 @@
 const quiz = require('./candidate-testing');
 let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let questions = [ 'Who was the first American woman in space? ',"True or false: 5000 meters = 5 kilometers. ","(5 + 3)/2 * 10 = ? ",
-'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ',"What is the minimum crew size for the ISS? " ];
+let questions = [ 'Who was the first American woman in space? ','True or false: 5000 meters = 5 kilometers. ','(5 + 3)/2 * 10 = ? ',"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",'What is the minimum crew size for the ISS? ' ];
 let candidateAnswers = [];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 
